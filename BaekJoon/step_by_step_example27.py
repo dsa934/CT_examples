54299e03b1ec821298c98ac13f9a80d9c7ddd20c
Jinwoo <overmind934@naver.com>
2021-06-21 13:53:30 +0900

    Baek 1783 - Knight  via usecase

--- BaekJoon/step_by_step_example27.py
+++ BaekJoon/step_by_step_example27.py
@@ -2,22 +2,20 @@
 
  Backjoon _ exampels #
 
-  ""   by Jinwoo Lee
-
-
- # Algorithm 
-
-  0. 
-
-  1. 
-
-  2.
-
-  3.
-
-
- # Attention for Implementation
-
+  "Baek 1783 - 병든 나이트 "   by Jinwoo Lee
 
 
 '''
+
+
+n, m = map(int, input().split())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(min(4, (m+1)//2))
+else:
+    if m <= 6:
+        print(min(4,m))
+    else:
+        print(m-2)
